c9949e46a742f564a1029b17d30743bf80ace6f1
Adrian Zabłocki <adrian.zablocki@polskieradio.pl>
2023-09-09 22:58:09 +0200

    Added filters query builder

--- backend/controllers/articleController.js
+++ backend/controllers/articleController.js
@@ -1,13 +1,31 @@
 import Article from '../models/article';
+import APIFlters from '../utils/APIFilters';
+
+export const getArticles = async(req, res, next) => {
+  const resPerPage = 2;
+  const articlesCount = await Article.estimatedDocumentCount();
+
+  const apiFilters = await new APIFlters(Article.find(), req.query)
+    .search()
+    .filter();
+
+  let articles = await apiFilters.query;
+  const filteredArticlesCount = articles.length;
+
+  apiFilters.pagination(resPerPage);
+
+  articles = await apiFilters.query.clone();
+
+  res.status(200).json({ 
+    resPerPage,
+    articlesCount,
+    filteredArticlesCount,
+    articles,
+  });
+}
 
 export const newArticle = async(req, res, next) => {
   const article = await Article.create(req.body);
 
   res.status(201).json({ article });
-}
-
-export const getArticles = async(req, res, next) => {
-  const articles = await Article.find();
-
-  res.status(200).json({ articles });
 }
