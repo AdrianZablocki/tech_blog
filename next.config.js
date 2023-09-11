e300969b7f179eb4ddb598b8b9626b0eac7b2542
Adrian Zabłocki <adrian.zablocki@polskieradio.pl>
2023-09-11 18:44:45 +0200

    Try

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    DB_URI: 'mongodb://127.0.0.1:27017/tech_blog',
+    DB_URI: 'mongodb+srv://appsynth:ipHJrbeE2pM46ps@atlascluster.f6unlqu.mongodb.net/?retryWrites=true&w=majority',
     API_URL: 'https://tech-blog-chi-bay.vercel.app',
     NEXTAUTH_URL: 'https://tech-blog-chi-bay.vercel.app'
   }
