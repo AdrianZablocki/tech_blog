3a83b0fbdc95717c3eb1e493425e7db7386b9897
Adrian Zabłocki <adrian.zablocki@polskieradio.pl>
2023-09-11 19:42:11 +0200

    Try

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_DB_URI: 'mongodb://127.0.0.1:27017/tech_blog',
-    NEXT_PUBLIC_API_URL: 'http://localhost:3000'
+    DB_URI: 'mongodb+srv://appsynth:ipHJrbeE2pM46ps@atlascluster.f6unlqu.mongodb.net/?retryWrites=true&w=majority',
+    API_URL: 'http://localhost:3000',
   }
 }
 
