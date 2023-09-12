c434d14f7bfbaa0ea8c81ec32975e438893be28e
Adrian Zabłocki <adrian.zablocki@polskieradio.pl>
2023-09-12 11:58:57 +0200

    Trying to hide confidential variables

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    DB_URI: 'mongodb://127.0.0.1:27017/tech_blog',
-    API_URL: 'http://localhost:3000'
+    NEXT_PUBLIC_DB_URI: 'mongodb://127.0.0.1:27017/tech_blog',
+    NEXT_PUBLIC_API_URL: 'http://localhost:3000'
   }
 }
 
