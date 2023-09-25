e089b5efdde8f7ab7f69e3941ee124c3a09fbd7f
Adrian Zabłocki <adrian.zablocki@polskieradio.pl>
2023-09-25 23:22:36 +0200

    Site map - try

--- robots.js
+++ robots.js
@@ -4,6 +4,6 @@ export default function robots() {
       userAgent: '*',
       allow: '/'
     },
-    // sitemap: 'https://appsynth.pl/sitemap.xml'
+    sitemap: 'https://appsynth.pl/sitemap.xml'
   }
 }
