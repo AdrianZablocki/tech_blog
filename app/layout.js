0f7a56c7f7c8caffa01e84e946463db3bf4502f2
Adrian Zabłocki <adrian.zablocki@polskieradio.pl>
2023-09-25 22:30:54 +0200

    Test seo

--- app/layout.js
+++ app/layout.js
@@ -2,11 +2,32 @@ import Header from '@/components/layouts/Header';
 import './globals.css';
 
 export const metadata = {
-  title: 'Tech_Blog',
-  description: 'Blog about the new technologies',
+  title: 'Appsynth',
+  description: 'Appsynth development',
   icons: {
     icon: '/favico.ico',
   },
+  openGraph: {
+    title: 'Appsynth',
+    locale: 'pl_PL',
+    type: 'website',
+    description: 'Appsynth development',
+    url: 'https://appsynth.pl',
+    // image: 'url to image'
+  },
+  robots: {
+    index: false,
+    follow: true,
+    nochache: true,
+    gogleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': ' large',
+      'max-snippet': -1
+    }
+  }
 }
 
 export default function RootLayout({ children }) {
@@ -19,3 +40,14 @@ export default function RootLayout({ children }) {
     </html>
   )
 }
+
+
+{/* 
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} /> 
+    
+    
+    
+    
+    */}
