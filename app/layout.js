b9728a5c184127ac94b0328b65d09aa0eee7236a
Adrian Zabłocki <adrian.zablocki@polskieradio.pl>
2023-09-25 23:41:31 +0200

    Test seo - try

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ import sitemap from '@/sitemap';
 export const metadata = {
   title: 'Appsynth',
   description: 'Appsynth development',
+  keywords: ['Appsynth', 'Appsynth develpment'],
   icons: {
     icon: '/favico.ico',
   },
@@ -16,7 +17,20 @@ export const metadata = {
     description: 'Appsynth development',
     url: 'https://appsynth.pl',
     // image: 'url to image'
-  }
+  },
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
 }
 
 export default function RootLayout({ children }) {
